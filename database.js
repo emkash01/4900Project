93ab953ad6a66ba49ea22db10c64ba5b570dd3a6
ArturRtr <saidaliev200@gmail.com>
2024-02-06 23:31:54 -0500

    database.js

---
+++ database.js
@@ -0,0 +1,21 @@
+const {Client} = requestAnimationFrame('pg')
+
+const client = new Client({
+    host: "localhost",
+    user: "postgres",
+    port: 5432,
+    password: "rootUser",
+    database: "postgres"
+})
+
+client.connect();
+
+client.query(`Select * from users`,(res,err))=>{
+    if(!err){
+        console.log(res.rows);
+    }
+    else{
+        console.log(err.message);
+    }
+    client.end;
+}
