b9e6f17c2a03ddb7b28f835414c40b16290512bb
ArturRtr <saidaliev200@gmail.com>
2024-02-07 01:21:22 -0500

    Create dbSetup.sql

---
+++ dbSetup.sql
@@ -0,0 +1,12 @@
+CREATE TABLE movie(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(255),--colum named name and text based column
+	actor VARCHAR(100),
+	rating DOUBLE PRECISION
+);
+
+INSERT INTO movie(name, actor, rating)
+VALUES('Oppenheimer','RDJ', 8.4);
+
+SELECT * FROM movie;
+
